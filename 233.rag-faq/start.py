f672a885f1dbbcd71ea70153d81c7cf0773b02c7
jo-narae <nare6004@gmail.com>
2025-09-08 19:02:25 +0900

    Refactor code structure for improved readability and maintainability

---
+++ 233.rag-faq/start.py
@@ -0,0 +1,196 @@
+## streamlit 관련 모듈 불러오기
+import streamlit as st
+from streamlit.runtime.uploaded_file_manager import UploadedFile
+
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_core.documents.base import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from langchain_core.runnables import Runnable
+from langchain.schema.output_parser import StrOutputParser
+from langchain_community.document_loaders import PyMuPDFLoader
+from typing import List
+import os
+import fitz  # PyMuPDF
+import re
+
+## 환경변수 불러오기
+from dotenv import load_dotenv,dotenv_values
+load_dotenv()
+
+
+
+############################### 1단계 : PDF 문서를 벡터DB에 저장하는 함수들 ##########################
+
+## 1: 임시폴더에 파일 저장
+def save_uploadedfile(uploadedfile: UploadedFile) -> str : 
+    temp_dir = "PDF_임시폴더"
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
+    file_path = os.path.join(temp_dir, uploadedfile.name)
+    with open(file_path, "wb") as f:
+        f.write(uploadedfile.read()) 
+    return file_path
+
+## 2: 저장된 PDF 파일을 Document로 변환
+def pdf_to_documents(pdf_path:str) -> List[Document]:
+    documents = []
+    loader = PyMuPDFLoader(pdf_path)
+    doc = loader.load()
+    for d in doc:
+        d.metadata['file_path'] = pdf_path
+    documents.extend(doc)
+    return documents
+
+## 3: Document를 더 작은 document로 변환
+def chunk_documents(documents: List[Document]) -> List[Document]:
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=100)
+    return text_splitter.split_documents(documents)
+
+## 4: Document를 벡터DB로 저장
+def save_to_vector_store(documents: List[Document]) -> None:
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_documents(documents, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+
+
+############################### 2단계 : RAG 기능 구현과 관련된 함수들 ##########################
+
+
+## 사용자 질문에 대한 RAG 처리
+@st.cache_data
+def process_question(user_question):
+
+
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    
+    ## 벡터 DB 호출
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+
+    ## 관련 문서 3개를 호출하는 Retriever 생성
+    retriever = new_db.as_retriever(search_kwargs={"k": 3})
+    ## 사용자 질문을 기반으로 관련문서 3개 검색 
+    retrieve_docs : List[Document] = retriever.invoke(user_question)
+
+    ## RAG 체인 선언
+    chain = get_rag_chain()
+    ## 질문과 문맥을 넣어서 체인 결과 호출
+    response = chain.invoke({"question": user_question, "context": retrieve_docs})
+
+    return response, retrieve_docs
+
+
+
+def get_rag_chain() -> Runnable:
+    template = """
+    다음의 컨텍스트를 활용해서 질문에 답변해줘
+    - 질문에 대한 응답을 해줘
+    - 간결하게 5줄 이내로 해줘
+    - 곧바로 응답결과를 말해줘
+
+    컨텍스트 : {context}
+
+    질문: {question}
+
+    응답:"""
+
+    custom_rag_prompt = PromptTemplate.from_template(template)
+    model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+    return custom_rag_prompt | model | StrOutputParser()
+
+
+
+############################### 3단계 : 응답결과와 문서를 함께 보도록 도와주는 함수 ##########################
+@st.cache_data(show_spinner=False)
+def convert_pdf_to_images(pdf_path: str, dpi: int = 250) -> List[str]:
+    doc = fitz.open(pdf_path)  # 문서 열기
+    image_paths = []
+    
+    # 이미지 저장용 폴더 생성
+    output_folder = "PDF_이미지"
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    for page_num in range(len(doc)):  #  각 페이지를 순회
+        page = doc.load_page(page_num)  # 페이지 로드
+
+        zoom = dpi / 72  # 72이 디폴트 DPI
+        mat = fitz.Matrix(zoom, zoom)
+        pix = page.get_pixmap(matrix=mat) # type: ignore
+
+        image_path = os.path.join(output_folder, f"page_{page_num + 1}.png")  # 페이지 이미지 저장 page_1.png, page_2.png, etc.
+        pix.save(image_path)  # PNG 형태로 저장
+        image_paths.append(image_path)  # 경로를 저장
+        
+    return image_paths
+
+def display_pdf_page(image_path: str, page_number: int) -> None:
+    image_bytes = open(image_path, "rb").read()  # 파일에서 이미지 인식
+    st.image(image_bytes, caption=f"Page {page_number}", output_format="PNG", width=600)
+
+
+def natural_sort_key(s):
+    return [int(text) if text.isdigit() else text for text in re.split(r'(\d+)', s)]
+
+def main():
+    # st.text(dotenv_values(".env"))
+    # st.text("셋팅완료")
+
+    st.set_page_config("아파트 청약 FAQ 챗봇", page_icon="🏠", layout="wide")
+
+    left_column, right_column = st.columns([1, 1])
+    with left_column:
+        st.header("아파트 청약 FAQ 챗봇")
+
+        pdf_doc = st.file_uploader("PDF 파일을 업로드 해주세요", type=["pdf"])
+        button = st.button("PDF 문서 저장")
+
+        if pdf_doc and button:
+            st.text("PDF 문서를 저장하고 있습니다. 잠시만 기다려주세요.")
+            pdf_path = save_uploadedfile(pdf_doc)
+            pdf_documents = pdf_to_documents(pdf_path)
+            smller_documents = chunk_documents(pdf_documents)
+            save_to_vector_store(smller_documents)
+
+            with st.spinner("PDF 문서를 이미지로 변환하는 중입니다..."):
+                images = convert_pdf_to_images(pdf_path)
+                st.session_state.images = images
+
+        user_question = st.text_input("질문을 입력해주세요", placeholder="예) 청약 1순위 조건이 어떻게 되나요?")
+
+        if user_question:
+            response, context = process_question(user_question)
+            # st.text(response)
+            # st.text(context)
+            st.text(response)
+
+            for document in context:
+                with st.expander("관련 문서"):
+                    st.text(document.page_content)
+                    file_path = document.metadata.get('source', '')
+                    page_number = document.metadata.get('page', 0) + 1
+                    button_key = f"link_{file_path}_{page_number}"
+                    reference_button = st.button(f"🔍 {os.path.basename(file_path)} pg.{page_number}", key=button_key)
+                    
+                    if reference_button:
+                        st.session_state.page_number = str(page_number)
+
+
+    with right_column:
+        page_number = st.session_state.get("page_number")
+        
+        if page_number:
+            page_number = int(page_number)
+            image_folder = "PDF_이미지"
+            images = sorted(os.listdir(image_folder), key=natural_sort_key)
+            print(images)
+            image_paths = [os.path.join(image_folder, img) for img in images]
+            print(page_number)
+            print(image_paths[page_number - 1])
+            display_pdf_page(image_paths[page_number - 1], page_number)
+
+if __name__ == "__main__":
+    main()
